2140304a009b4b4c39e769ed3754e10879b8bbb6
Kathleen Jocson <kgjocson@up.edu.ph>
2026-07-25 22:33:53 +0800

    feat: Update and delete post and user

--- schemas.py
+++ schemas.py
@@ -16,6 +16,13 @@ class UserResponse(UserBase):
     image_file: str | None
     image_path: str
 
+class UserUpdate(BaseModel):
+    username: str | None = Field(default=None, min_length=1, max_length=50)
+    email: EmailStr | None = Field(default=None, max_length=120)
+    image_file: str | None = Field(default=None, max_length=200)
+
+
+
 
 class PostBase(BaseModel):
     title: str = Field(min_length=1, max_length=100)
@@ -24,6 +31,11 @@ class PostBase(BaseModel):
 
 class PostCreate(PostBase):
     user_id: int # temporary solution, will be replaced with authentication
+
+
+class PostUpdate(BaseModel):
+    title: str | None = Field(default=None, min_length=1, max_length=100)
+    content: str | None = Field(default=None, min_length=1)
 
 class PostResponse(PostBase):
    model_config = ConfigDict(from_attributes=True)
